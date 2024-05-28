cff996b6ade23512df357478058fbe1f208ca8e9
HadiHarmandar <hadiharmandar1@gmail.com>
2024-05-28 15:27:19 -0500

    day03 practices

---
+++ src/day03_practices/Rectangle.java
@@ -0,0 +1,26 @@
+package day03_practices;
+
+public class Rectangle {
+
+    public static void main(String[] args) {
+
+        double width = 2;
+        double length = 3;
+        double area = width * length;
+
+        System.out.println("area = " + area);
+
+    }
+}
+
+/**
+ Create a class named Rectangle.java
+ Declare the following variables:
+
+     1. width
+     2. Length
+
+ Write a program that can display the area of the rectangle
+
+    Hints: area = width * length
+ */
