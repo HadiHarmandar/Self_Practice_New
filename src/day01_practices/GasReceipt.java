6aa45dd4fb09b8d0eb707a63d7a98825fdb8378c
HadiHarmandar <hadiharmandar1@gmail.com>
2024-05-28 11:07:20 -0500

    day01 practices

---
+++ src/day01_practices/GasReceipt.java
@@ -0,0 +1,16 @@
+package day01_practices;
+
+public class GasReceipt {
+
+    public static void main(String[] args) {
+
+        System.out.println("\t MCLEAN STORE\t");
+        System.out.println("04-29-2022\t02:45 pm");
+        System.out.println();
+        System.out.println("Gallons:\t\t11.48");
+        System.out.println("Price/gallon:\t$4.15");
+        System.out.println();
+        System.out.println("Fuel total:\t\t$47.642");
+
+    }
+}
---
+++ src/day01_practices/MyClassmates.java
@@ -0,0 +1,15 @@
+package day01_practices;
+
+public class MyClassmates {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ayse Vurmaz");
+        System.out.println("Igor Chuvashov");
+        System.out.println("Tanya Elliott");
+        System.out.println("Brandon Vernon");
+        System.out.println("Salma Sali");
+
+    }
+
+}
